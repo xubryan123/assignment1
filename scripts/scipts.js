2c291216d924f1f79a88bbe08c98803db3c46c51
Bryan Xu <xubryan123@gmail.com>
2021-04-07 18:17:37 -0700

    added assignment 2 javascript comments

---
+++ scripts/scipts.js
@@ -0,0 +1,16 @@
+/*dark mode function: 
+-create dark mode class within css 
+-set variable to body element using document.body
+-toggle dark mode class using element.classList.toggle()
+
+hide buttons function: 
+-use js to access the button using document.getElementById()
+-set the button to change display property to none onclick using .style.display = 'none'
+
+save button function:
+-use console.log to prompt user to enter title of note
+-add object to object array
+-return title name in variable
+-select nav bar using document.getElementById()
+-create new element using document.createElement() using title name variable
+-add to nav using .appendChild()*/
